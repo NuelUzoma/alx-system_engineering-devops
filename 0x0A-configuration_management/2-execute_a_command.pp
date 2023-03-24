49af59bba17730390b5dbb0f75f560524e6b8cd8
NuelUzoma <emcool17@gmail.com>
2023-03-24 23:31:51 +0100

    Exec command in a manifest file using Puppet

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# Using Puppet, craete a manifest that kills a process named "killmenow"
+exec { 'killmenow' :
+    command => 'pkill killmenow',
+    path    => ['/usr/local/bin', '/usr/bin', '/bin']
+}
