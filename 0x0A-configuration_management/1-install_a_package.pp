91d2b143ec881880339705a81408ebe9cf606ded
NuelUzoma <emcool17@gmail.com>
2023-03-24 22:25:44 +0100

    Flask Package Installation with Puppet Manifest

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Using Puppet, install Flask from pip3
-exec { 'flask':
-    command => 'pip3 install flask',
-    path    => ['/usr/local/bin', '/usr/bin', '/bin'],
-    unless  => 'pip3 show flask'
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3'
 }
