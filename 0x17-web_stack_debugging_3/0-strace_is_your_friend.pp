d051e3e1528884f5753cb62536e744539c8d266b
NuelUzoma <emcool17@gmail.com>
2023-05-11 01:15:06 +0100

    Fixed Typo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Using strace, find out why Apache is returning a 500 error
 file { '/etc/apache2/sites-available/default-ssl.conf':
   ensure  => present,
   content => 'new contents of the file',
