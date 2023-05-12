4640297204605e4e4477e30a4b7b4089c578c21d
NuelUzoma <emcool17@gmail.com>
2023-05-13 00:51:47 +0100

    Fixed Typo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Using strace, find out why Apache is returning a 500 error
-exec { 'var/www/html/wp-settings.php': 
-   command => "sed -i 's/phpp/php/g' var/www/html/wp-settings.php", 
-   path    => '/usr/bin:/usr/sbin:/bin'
+exec { 'var/www/html/wp-settings.php':
+        command => "sed -i 's/phpp/php/g' var/www/html/wp-settings.php",
+        path    => '/usr/bin:/usr/sbin:/bin'
 }
